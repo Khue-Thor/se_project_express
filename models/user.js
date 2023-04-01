123788b762ce519876aef6b26147e56a2c82b152
khuephamy <khuephumy19@gmail.com>
2023-04-01 13:01:59 -0500

    update user schema

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -11,6 +12,12 @@ const userSchema = mongoose.Schema({
   avatar: {
     type: String,
     required: true,
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: 'You must enter a valid URL',
+    }
   }
 })
 
