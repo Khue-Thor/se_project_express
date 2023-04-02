da036951ea836a0c1aa08d8f88ebe5f021289e59
khuephamy <khuephumy19@gmail.com>
2023-04-02 13:01:56 -0500

    getItem controller

--- controllers/clothingItem.js
+++ controllers/clothingItem.js
@@ -1,2 +1,10 @@
 const ClothingItem = require("../models/clothingItem");
 
+const getItems = (req, res, nex) => {
+  ClothingItem.find({})
+    .then((items) => res.status(200).send(items))
+    .catch((err) => {
+      next(err);
+    })
+}
+
