8cf3c70c03fe3e1b43eae86f27447c554a6999f8
khuephamy <khuephumy19@gmail.com>
2023-04-02 13:02:22 -0500

    createItem controller

--- controllers/clothingItem.js
+++ controllers/clothingItem.js
@@ -8,3 +8,20 @@ const getItems = (req, res, nex) => {
     })
 }
 
+const createItem = (req, res, next) => {
+  const userId = req.user._id;
+
+  const {name, weather, imageUrl} = req.body;
+
+  ClothingItem.create({name, weather, imageUrl, owner: userId})
+    .then((item) => {
+      res.status(200).send(item);
+    })
+    .catch((err) => {
+      if(err.name === "ValidationError") {
+        res.status(401).send({ message: "Invalid Data"})
+      } else {
+        next(err)
+      }
+    })
+}
