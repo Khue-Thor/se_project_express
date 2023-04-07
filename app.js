0bd6abc18edd9b2a81e2ff1d188b096c400486fe
khuephamy <khuephumy19@gmail.com>
2023-04-07 12:24:04 -0500

    removed the hardcoded middleware _id

--- app.js
+++ app.js
@@ -1,29 +1,22 @@
-const express = require('express');
+const express = require("express");
 
 const cors = require("cors");
 
 const app = express();
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { PORT = 3001 } = process.env;
 
 // DataBase Connection
-mongoose.connect('mongodb://localhost:27017/wtwr_db');
+mongoose.connect("mongodb://localhost:27017/wtwr_db");
 
-const routes = require('./routes/index');
+const routes = require("./routes/index");
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '642b17b2e491567c21d7c327',
-  };
-  next();
-});
-
-app.use('/', routes);
+app.use("/", routes);
 
 app.listen(PORT, () => {
   console.log(`Connection is listen to ${PORT}`);
