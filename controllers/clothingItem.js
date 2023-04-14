3233b6aa3c4ec25b73cddde66361883a6715cae5
khuephamy <khuephumy19@gmail.com>
2023-04-14 11:15:24 -0500

    feature deleteItem Controller

--- controllers/clothingItem.js
+++ controllers/clothingItem.js
@@ -40,7 +40,7 @@ const deleteItem = (req, res) => {
     .orFail()
     .then((item) => {
       if (item.owner.equals(req.user._id)) {
-        return item.deleteOne(() => res.send({ clothingItem: item }));
+        return item.deleteOne().then(() => res.send({ ClothingItem: item }));
       }
       return res.status(STATUS_CODES.Forbidden).send({ message: "Forbidden" });
     })
