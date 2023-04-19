0523f788f530648e72de5160414df454ecf094c2
khuephamy <khuephumy19@gmail.com>
2023-04-19 14:31:37 -0500

    feature validation

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,24 +1,33 @@
 const { celebrate, Joi } = require("celebrate");
-const validator = require('validator');
+const validator = require("validator");
 
 const validateURL = (value, helpers) => {
   if (validator.isURL(value)) {
     return value;
   }
   return helpers.error("string.uri");
-}
-
+};
 
 const validateUserBody = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30).message({
-      "string.min":
-    })
-  })
-})
-
-
-
+      "string.min": 'The minimum length of the "name" field is 2',
+      "string.max": 'The maximum length of the "name" field is 30',
+      "string.empty": 'The "name" field must be filled in',
+    }),
+    avatar: Joi.string().required().custom(validateURL).message({
+      "string.empty": 'The "imageUrl" field must be filled in',
+      "string.uri": 'the "imageUrl" field must be a valid url',
+    }),
+    email: Joi.string().required().email().message({
+      "string.empty": 'The "email" field cannot be empty',
+      "string.email": 'The "email" field must be a valid email',
+    }),
+    password: Joi.string().required().messages({
+      "string.empty": 'The "password" field cannot be empty',
+    }),
+  }),
+});
 
 const validateCardBody = celebrate({
   body: Joi.object().keys({
@@ -35,7 +44,25 @@ const validateCardBody = celebrate({
   }),
 });
 
+const validateUserLogin = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email().messages({
+      "string.empty": 'The "email" field cannot be empty',
+      "string.email": 'The "email" field must be a valid email',
+    }),
+    password: Joi.string().required().messages({
+      "string.empty": 'The "password" field cannot be empty',
+    }),
+  }),
+});
 
+const validateIds = celebrate({
+  body: Joi.object().keys({
+    params: Joi.object().keys({
+      id: Joi.string().length(24).hex().required(),
+    }),
+  }),
+});
 
 
 module.exports = {
